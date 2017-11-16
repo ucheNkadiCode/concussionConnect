30aef9dd4f06c2aeda9319e5922fe8f2b76f6362
Uche Nkadi <unkadi@rsglab.com>
2017-11-16 15:42:06 -0500

    Added Amazon Web Service Registration

--- app/src/main/java/com/github/concussionconnect/Controller/LoginActivity.java
+++ app/src/main/java/com/github/concussionconnect/Controller/LoginActivity.java
@@ -11,6 +11,13 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoDevice;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserSession;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.AuthenticationContinuation;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.AuthenticationDetails;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.ChallengeContinuation;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.MultiFactorAuthenticationContinuation;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.handlers.AuthenticationHandler;
 import com.github.concussionconnect.R;
 
 /**
@@ -67,6 +74,48 @@ public class LoginActivity extends Activity implements View.OnClickListener {
 //            return;
 //        }
 //        Toast.makeText(getApplicationContext(), "User would be logged in", Toast.LENGTH_SHORT).show();
+// Callback handler for the sign-in process
+        AuthenticationHandler authenticationHandler = new AuthenticationHandler() {
+
+            @Override
+            public void onSuccess(CognitoUserSession cognitoUserSession, CognitoDevice device) {
+                // Sign-in was successful, cognitoUserSession will contain tokens for the user
+            }
+
+            @Override
+            public void getAuthenticationDetails(AuthenticationContinuation authenticationContinuation, String userId) {
+                // The API needs user sign-in credentials to continue
+//                String userId = editTextEmail.getText().toString().trim();
+                String password = editTextPassword.getText().toString().trim();
+                AuthenticationDetails authenticationDetails = new AuthenticationDetails(userId, password, null);
+
+                // Pass the user sign-in credentials to the continuation
+                authenticationContinuation.setAuthenticationDetails(authenticationDetails);
+
+                // Allow the sign-in to continue
+                authenticationContinuation.continueTask();
+            }
+
+            @Override
+            public void getMFACode(MultiFactorAuthenticationContinuation multiFactorAuthenticationContinuation) {
+//                // Multi-factor authentication is required; get the verification code from user
+//                multiFactorAuthenticationContinuation.setMfaCode(mfaVerificationCode);
+//                // Allow the sign-in process to continue
+//                multiFactorAuthenticationContinuation.continueTask();
+            }
+
+            @Override
+            public void onFailure(Exception exception) {
+                // Sign-in failed, check exception for the cause
+            }
+            @Override
+            public void authenticationChallenge(ChallengeContinuation challenge) {
+                //Nothing
+            }
+        };
+
+// Sign in the user
+//        cognitoUser.getSessionInBackground(authenticationHandler);
         finish();
         startActivity(new Intent(this, MainActivity.class));
     }
--- app/src/main/java/com/github/concussionconnect/Controller/RegisterActivity.java
+++ app/src/main/java/com/github/concussionconnect/Controller/RegisterActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -13,6 +14,16 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.auth.AnonymousAWSCredentials;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUser;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserAttributes;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserCodeDeliveryDetails;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserPool;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.handlers.SignUpHandler;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.cognitoidentityprovider.AmazonCognitoIdentityProviderClient;
 import com.github.concussionconnect.R;
 
 import static java.lang.String.valueOf;
@@ -23,7 +34,6 @@ public class RegisterActivity extends Activity implements View.OnClickListener {
     private EditText editTextFirstName;
     private EditText editTextLastName;
     private EditText editTextPassword;
-    private Spinner spinnerUserType;
     private TextView textLoginHere;
     private Button buttonRegister;
 
@@ -37,10 +47,6 @@ public class RegisterActivity extends Activity implements View.OnClickListener {
         editTextPassword = (EditText) findViewById(R.id.editTextPassword);
         textLoginHere = (TextView) findViewById(R.id.textLoginHere);
         buttonRegister = (Button) findViewById(R.id.buttonRegister);
-        spinnerUserType = (Spinner) findViewById(R.id.spinnerUserType);
-        ArrayAdapter<CharSequence> userTypeAdapter = ArrayAdapter.createFromResource(this, R.array.user_type, android.R.layout.simple_spinner_item);
-        userTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerUserType.setAdapter(userTypeAdapter);
         buttonRegister.setOnClickListener(this);
         textLoginHere.setOnClickListener(this);
 
@@ -48,13 +54,7 @@ public class RegisterActivity extends Activity implements View.OnClickListener {
 
     public void userRegister() {
         //For now, when they click register, it'll take you into the test as a trainer
-//        String email = editTextEmail.getText().toString().trim();
-//        String password = editTextPassword.getText().toString().trim();
-//        String firstName = editTextFirstName.getText().toString().trim();
-//        String lastName = editTextLastName.getText().toString().trim();
-//        String userType = valueOf(spinnerUserType.getSelectedItem());
-
-//        if (TextUtils.isEmpty(email)) {
+        //        if (TextUtils.isEmpty(email)) {
 //            Toast.makeText(this, "Please enter your e-mail", Toast.LENGTH_LONG).show();
 //            return;
 //        }
@@ -79,8 +79,54 @@ public class RegisterActivity extends Activity implements View.OnClickListener {
 //            return;
 //        }
 //        Toast.makeText(getApplicationContext(), "User would be registered", Toast.LENGTH_LONG).show();
-        finish();
-        startActivity(new Intent(this, MainActivity.class));
+//        AmazonCognitoIdentityProviderClient identityProviderClient = new AmazonCognitoIdentityProviderClient(new AnonymousAWSCredentials(), new ClientConfiguration());
+//        identityProviderClient.setRegion(Region.getRegion(Regions.US_EAST_1));
+//        CognitoUserPool pool = new CognitoUserPool(context, USER_POOL_ID, APP_CLIENT_ID, APP_CLIENT_SECRET, identityProviderClient);
+        String poolId = "us-east-2_3UQW3UkEt";
+        String clientId = "780etbq74568h03k5uphag3e8a";
+        String clientSecret = "t0l4a1o6mhmmqflvp1pbtinum7l5fstthvvvndthftd2n6r27ol";
+        CognitoUserPool userPool = new CognitoUserPool(getApplicationContext(), poolId, clientId, clientSecret, Regions.US_EAST_2);
+        CognitoUserAttributes userAttributes = new CognitoUserAttributes();
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        if (password.length() < 6) {
+            Toast.makeText(getApplicationContext(), "Password length must be 6 or greater", Toast.LENGTH_LONG).show();
+        }
+        //primary key hashcode, Given Name, Last Name, Birthdate
+        String givenName = editTextFirstName.getText().toString().trim();
+        String familyName = editTextLastName.getText().toString().trim();
+        userAttributes.addAttribute("given_name", givenName);
+        userAttributes.addAttribute("family_name", familyName);
+        userAttributes.addAttribute("email", email);
+
+        SignUpHandler signupCallback = new SignUpHandler() {
+            @Override
+            public void onSuccess(CognitoUser cognitoUser, boolean userConfirmed, CognitoUserCodeDeliveryDetails cognitoUserCodeDeliveryDetails) {
+                Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_LONG).show();
+                // Sign-up was successful
+                // Check if this user (cognitoUser) needs to be confirmed
+                if(!userConfirmed) {
+                    // This user must be confirmed and a confirmation code was sent to the user
+                    // cognitoUserCodeDeliveryDetails will indicate where the confirmation code was sent
+                    // Get the confirmation code from user
+                }
+                else {
+                    // The user has already been confirmed
+                }
+
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+
+            }
+
+            @Override
+            public void onFailure(Exception exception) {
+                // Sign-up failed, check exception for the cause
+                Toast.makeText(getApplicationContext(), "FAILURE", Toast.LENGTH_LONG).show();
+            }
+        };
+//        Log.wtf("myWTFTag", "HELLO BRO");
+        userPool.signUpInBackground(email, password, userAttributes, null, signupCallback);
+//        finish();
 
     }
     @Override
@@ -94,4 +140,42 @@ public class RegisterActivity extends Activity implements View.OnClickListener {
             startActivity(new Intent(this, LoginActivity.class));
         }
     }
+
+//    String sha1Hash( String toHash )
+//    {
+//        String hash = null;
+//        try
+//        {
+//            MessageDigest digest = MessageDigest.getInstance( "SHA-1" );
+//            byte[] bytes = toHash.getBytes("UTF-8");
+//            digest.update(bytes, 0, bytes.length);
+//            bytes = digest.digest();
+//
+//            // This is ~55x faster than looping and String.formating()
+//            hash = bytesToHex( bytes );
+//        }
+//        catch( NoSuchAlgorithmException e )
+//        {
+//            e.printStackTrace();
+//        }
+//        catch( UnsupportedEncodingException e )
+//        {
+//            e.printStackTrace();
+//        }
+//        return hash;
+//    }
+//
+//    // http://stackoverflow.com/questions/9655181/convert-from-byte-array-to-hex-string-in-java
+//    final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
+//    public static String bytesToHex( byte[] bytes )
+//    {
+//        char[] hexChars = new char[ bytes.length * 2 ];
+//        for( int j = 0; j < bytes.length; j++ )
+//        {
+//            int v = bytes[ j ] & 0xFF;
+//            hexChars[ j * 2 ] = hexArray[ v >>> 4 ];
+//            hexChars[ j * 2 + 1 ] = hexArray[ v & 0x0F ];
+//        }
+//        return new String( hexChars );
+//    }
 }
